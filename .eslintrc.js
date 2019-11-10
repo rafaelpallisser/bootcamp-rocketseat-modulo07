da0fe7a00c41e6013eba4a69cfec3fbd5f821039
Rafael Pallisser de Oliveira <rafaelpallisser@gmail.com>
2019-11-10 15:37:52 -0300

    Reactotron + Redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
     'react/require-default-props': 'off',
-    'jsx-a11y/control-has-associated-label': 'off'
+    'jsx-a11y/control-has-associated-label': 'off',
+    'no-console': ["error", { allow: ["tron"] }]
   },
 };
